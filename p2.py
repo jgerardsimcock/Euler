a6ea7627a1e31c5c70a3ad5b856bfb68ae508a47
jgerardsimcock <justin_simcock@yahoo.com>
2015-11-29 09:33:27 -0800

    update p2

--- p2.py
+++ p2.py
@@ -1,13 +1,28 @@
 def fibSeq(upperBoundary):
-  return_value = 0
+  fib = 1
+  fib2 = 2
+  temp = 0
+  total = 0
 
-  for val in range(1, upperBoundary+1):
-    if val % 2 == 0:
-      return_value += val
-      #print val
-  print return_value
+  while temp <= upperBoundary:
+      temp = fib2
+      if not temp % 2:
+        total += temp
 
-    #print return_value
-    #return return_value 
+      fib, fib2 = fib2, fib + fib2
+
+  print total
+
 
 fibSeq(4000000)
+
+
+def even_fib(limit):
+  a, b = 0, 1
+  while a < limit:
+    if not a % 2:
+        yield a
+
+    a, b = b, a + b
+
+#print sum(even_fib(4000000))
